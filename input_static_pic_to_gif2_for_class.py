355f5e716b57d2c0b2cba9a072d6d9b7aed67a11
李远君 <1123431949@qq.com>
2019-03-22 09:14:13 +0800

    Update input_static_pic_to_gif2_for_class.py

--- input_static_pic_to_gif2_for_class.py
+++ input_static_pic_to_gif2_for_class.py
@@ -133,7 +133,7 @@ class FaceDetect(object):
 
     def get_cigarette_info(self, face_shape, face_width):
         """
-        获取当前面部的眼镜信息
+        获取当前面部的烟卷信息
         :param face_shape:
         :param face_width:
         :return:
